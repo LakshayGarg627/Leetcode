89ac097dbc0a932ec7f4ce8433fdb881019403ea
LakshayGarg627 <lakshaygarg627@gmail.com>
2024-10-07 19:20:14 +0530

    new commit

---
+++ EASY/ques1512.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ques1512{
+    public static int numIdenticalPairs(int nums[]){
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String args[]){
+        int nums[]={1,2,3,1,1,3};
+        System.out.println(numIdenticalPairs(nums));    
+        }
+}
--- EASY/ques1572.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class ques1572{
-    public 
-    public static void main(String args[] ){
-        
-    }
-}
